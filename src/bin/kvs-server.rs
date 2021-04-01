d53f9d461d96cfd7f98697497c3a0365361e66d3
lighk <daoshibushi@gmail.com>
2021-04-01 22:52:14 +0800

    feat(kvs-server):add log when start it

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -2,15 +2,17 @@ use clap::arg_enum;
 use structopt::StructOpt;
 use std::net::SocketAddr;
 use std::fmt::{Display, Formatter};
-use log::{error, info, warn};
+use log::{error, info, warn, debug};
 use log::LevelFilter;
 use std::env::current_dir;
-use kvs::{Result, KvsError};
+use kvs::*;
 use std::fs;
+use std::process::exit;
 
 
 const DEFAULT_ADDR: &str = "127.0.0.1:4000";
 const DEFAULT_ENGINE: Engine = Engine::kvs;
+const ENGINE_FILE_NAME: &str = "engine";
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "kvs-server", about = "A key-value storage server.")]
@@ -25,7 +27,7 @@ struct Opt {
     addr: SocketAddr,
     #[structopt(
     long,
-    help = "Set storage engine, either kvs or sled.",
+    help = "Set storage engines, either kvs or sled.",
     possible_values = & Engine::variants(),
     value_name = "ENGINE-NAME",
     )]
@@ -42,17 +44,37 @@ arg_enum! {
 }
 
 fn main() {
-    env_logger::builder().init();
-    let opt = Opt::from_args() as Opt;
-    info!("kvs-server {}", env!("CARGO_PKG_VERSION"));
-    info!("listening on {}", opt.addr);
-    info!("use {} engine", opt.engine.unwrap_or(DEFAULT_ENGINE));
+    env_logger::builder().filter_level(LevelFilter::Debug).init();
+    let mut opt = Opt::from_args() as Opt;
+    let result = previous_engine()
+        .and_then(|previous_engine| {
+            if opt.engine.is_none() {
+                opt.engine = previous_engine;
+            }
+            debug!("engine: current={:?}, previous={:?}", opt.engine, previous_engine);
 
-    let mut engine;
+            if previous_engine.is_some() && previous_engine != opt.engine {
+                error!("The storage engine {} has been set up and cannot be replaced",
+                       previous_engine.unwrap());
+                exit(1);
+            }
+            let engine = opt.engine.unwrap_or(DEFAULT_ENGINE);
+            info!("kvs-server {}", env!("CARGO_PKG_VERSION"));
+            info!("listening on {}", opt.addr);
+            info!("use {} engines", engine);
+
+            //save engine type.
+            fs::write(current_dir()?.join(ENGINE_FILE_NAME), format!("{}", engine))?;
+            Ok(())
+        });
+    if let Err(e) = result {
+        error!("{}", e);
+        exit(1);
+    }
 }
 
 fn previous_engine() -> Result<Option<Engine>> {
-    let engine_path = current_dir()?.join("engine");
+    let engine_path = current_dir()?.join(ENGINE_FILE_NAME);
     if !engine_path.exists() {
         return Ok(None);
     }
@@ -60,8 +82,8 @@ fn previous_engine() -> Result<Option<Engine>> {
     match fs::read_to_string(engine_path)?.parse() {
         Ok(engine) => Ok(Some(engine)),
         Err(e) => {
-            error!("Invalid engine: {}", e);
-            KvsError::InvalidConfig
+            error!("Invalid engines: {}", e);
+            Err(KvsError::ServerStart)
         }
     }
 }
